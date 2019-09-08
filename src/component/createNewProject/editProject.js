87b1802d87ae6a6057e80e70601ab0d7265c1350
aras armani <aras.armani2014@gmail.com>
2019-09-08 20:03:41 +0300

    edit project is added

---
+++ src/component/createNewProject/editProject.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import SweetAlert from 'sweetalert-react';
+
+
+
+import './style.css';
+
+
+class EditPRojectComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div className="">
+                 
+            </div>
+         );
+    }
+}
+ 
+export default EditPRojectComponent;
