c534efd4077e5942051e0c90d2281ee20e2b0864
aras armani <aras.armani2014@gmail.com>
2019-09-09 09:57:33 +0300

    buttons is added

--- src/component/createNewProject/editProject.js
+++ src/component/createNewProject/editProject.js
@@ -24,7 +24,41 @@ class EditPRojectComponent extends Component {
                     ویرایش پروژه
                 </div>
                 <div className="card-container">
-                    <h1>hwlloooo</h1>
+                    <div className="CNP-project-title">
+                        <h2>عنوان پروژه</h2>
+                        <Input
+                            type={'text'}
+                            name={'title'}
+                            placeholder={'عنوان پروژه '}
+                            changed={this.changedHandler}
+                            error={this.state.errorTitle}
+                            val={this.state.title}
+                        />
+                    </div>
+
+                    <div className="CNP-project-title">
+                        <h2>اطلاعات مربوط به پروژه</h2>
+                        <TextArea
+                            type={'text'}
+                            name={'description'}
+                            placeholder={'اطلاعات مربوط به پروژه'}
+                            changed={this.changedHandler}
+                            error={this.state.errorDescription}
+                            val={this.state.description}
+                        />
+                    </div>
+
+                    <div className="CNP-project-title">
+                        <h2>اطلاعات مربوط به طراح</h2>
+                        <TextArea
+                            type={'text'}
+                            name={'otherDescription'}
+                            placeholder={'اطلاعات که طراح باید بداند'}
+                            changed={this.changedHandler}
+                            error={this.state.errorOtherDescription}
+                            val={this.state.otherDescription}
+                        />
+                    </div> 
                 </div>
             </div>
          );
