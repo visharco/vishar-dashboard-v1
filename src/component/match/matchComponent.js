9e76e9cb3800ce238d0b795f7ffffe25d2046a00
aras armani <aras.armani2014@gmail.com>
2019-10-13 19:46:12 +0300

    match component is added

---
+++ src/component/match/matchComponent.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class MatchComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div>
+                <h1>Match component</h1>
+            </div>
+         );
+    }
+}
+ 
+export default MatchComponent;
