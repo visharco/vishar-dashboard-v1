bae4d517f2aa4946a2f463405bd43a335645911b
aras armani <aras.armani2014@gmail.com>
2019-10-16 23:31:32 +0300

    input added

--- src/component/match/matchComponent.js
+++ src/component/match/matchComponent.js
@@ -30,6 +30,13 @@ class MatchComponent extends Component {
                         error={this.state.forgetEmailError} // if you want show error pass error text to this props
                     />
 
+<Input 
+                        type={'text'} 
+                        name={'email'}
+                        placeholder={'Email'}
+                        changed={this.changedHandler}
+                        error={this.state.forgetEmailError} // if you want show error pass error text to this props
+                    />   
                     </div>
                     <div className="col-50">dddd</div>
                 </div>
