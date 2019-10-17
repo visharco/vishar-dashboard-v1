481ebd57827b8e98273d8cdcaede5a90f82a04a5
aras armani <aras.armani2014@gmail.com>
2019-10-17 19:13:11 +0300

    match form dara init is compalted

--- src/component/match/matchComponent.js
+++ src/component/match/matchComponent.js
@@ -9,6 +9,15 @@ class MatchComponent extends Component {
         super(props);
         this.state = {  }
     }
+
+
+    onBlur = (evt,name) => {
+        console.log("onBlur event called with event info: ", evt);
+        console.log(this.state[name])
+      }
+
+
+
     render() { 
         return ( 
             <div className="Message" >
@@ -19,26 +28,62 @@ class MatchComponent extends Component {
                     <div className="col-50">
                     <Input 
                         type={'text'} 
-                        name={'email'}
-                        placeholder={'Email'}
+                        name={'title'}
+                        placeholder={'عنوان مسابقه'}
                         changed={this.changedHandler}
                         error={this.state.forgetEmailError} // if you want show error pass error text to this props
                     />
                   
-
-                  <CKEditor 
-                            name='content'
-                            activeClass="p10" 
-                            content={this.state.content} 
-                            events={{
-                                "blur": (e) => this.onBlur(e,'content'),
-                                "afterPaste": this.afterPaste,
-                                "change":(e) => this.onChange(e,'content')
-                            }}
-                        />
-                        
+                    <p>توضیحات مسابقه</p>
+                    <div className="ckeditor-container">
+                        <CKEditor 
+                                name='content'
+                                activeClass="p10" 
+                                content={this.state.content} 
+                                
+                                config={{
+                                    // removeButtons: 'Styles, about, Format',
+                                    // toolbar: [ [ 'Cut','Copy','Paste' ,'Bold','Italic' , 'Underline', 'List' ] ],
+                                    language : ['fa']
+                                }}
+                                events={{
+                                    "blur": (e) => this.onBlur(e,'content'),
+                                    "afterPaste": this.afterPaste,
+                                    "change":(e) => this.onChange(e,'content')
+                                }}
+                            />
                     </div>
-                    <div className="col-50">dddd</div>
+                    <Input 
+                        type={'text'} 
+                        name={'title'}
+                        placeholder={'بودجه برگزاری'}
+                        changed={this.changedHandler}
+                        error={this.state.forgetEmailError} // if you want show error pass error text to this props
+                    />
+                    <Input 
+                        type={'text'} 
+                        name={'title'}
+                        placeholder={'تلفن تماس'}
+                        changed={this.changedHandler}
+                        error={this.state.forgetEmailError} // if you want show error pass error text to this props
+                    />
+                    <Input 
+                        type={'text'} 
+                        name={'title'}
+                        placeholder={'پست الکترونیک'}
+                        changed={this.changedHandler}
+                        error={this.state.forgetEmailError} // if you want show error pass error text to this props
+                    />
+                    <Input 
+                        type={'text'} 
+                        name={'title'}
+                        placeholder={'نشانی وب سایت'}
+                        changed={this.changedHandler}
+                        error={this.state.forgetEmailError} // if you want show error pass error text to this props
+                    />
+
+                    </div>
+                    <div className="col-50">attach file palce</div>
                 </div>
             </div>
          );
